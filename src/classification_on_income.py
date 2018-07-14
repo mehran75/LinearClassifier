54c2523fe4815c002f878b4c5fbea55f8a495f7a
mehran <mehranrafiee5@gmail.com>
2018-07-14 20:49:45 +0430

    Initial commit

---
+++ src/classification_on_income.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+
+
+# census = pd.read_csv('cal_housing_clean.csv')
+
+# census.head()
+
+import seaborn as sns
